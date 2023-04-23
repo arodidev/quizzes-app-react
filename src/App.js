e72b58c7b0dc8f8cb0e53625ae296200149d84ae
arodidev <51090527+arodidev@users.noreply.github.com>
2023-04-23 18:03:32 +0300

    Fetch request added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+
+  const [quiz, setQuiz] = React.useState([])
+
+  React.useEffect(()=>{
+    fetch("https://the-trivia-api.com/v2/questions")
+      .then(res => res.json())
+      .then(data=> setQuiz(data))
+  },[])
+
+  const logged = quiz.map((eachQuiz)=>{
+      return(
+      <p>
+        {eachQuiz}
+      </p>
+      )
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {logged}
     </div>
   );
 }
