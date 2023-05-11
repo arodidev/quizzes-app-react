79fe047f975cba668223b246519af8e81d3cba07
arodidev <arodi745@gmail.com>
2023-05-11 20:12:32 +0300

    Refactoring of the broken code
    Also added a Question component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
 import React from 'react';
+import Question from './components/Question';
 
 function App() {
 
   const [quiz, setQuiz] = React.useState([])
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     fetch("https://the-trivia-api.com/v2/questions")
-      .then(res => res.json())
-      .then(data=> setQuiz(data))
-  },[])
-
-  const logged = quiz.map((eachQuiz)=>{
-      return(
-      <p>
-        {eachQuiz}
-      </p>
-      )
-  })
-
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setQuiz(data)
+    })
+  }, [])
+  
   return (
     <div className="App">
-      {logged}
+      { quiz.length > 0 ? 
+        <>
+        {
+          quiz.map((eachObj)=>{
+            return <Question key = {eachObj.id} {...eachObj} />
+          })
+        }
+        </>
+        : <h1>Page Loading...</h1> }
     </div>
   );
 }
---
+++ src/components/Question.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+export default function Question(props) {
+
+    const [answerState, setAnswerState] = React.useState(false)
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+        return array;
+    }
+
+    function showAnswer() {
+        setAnswerState(true)
+    }
+    
+    let incorrectAnswers = props.incorrectAnswers
+    const correctAnswer = props.correctAnswer
+    incorrectAnswers.push(correctAnswer)
+
+    const answerSet = new Set(incorrectAnswers)
+    incorrectAnswers = [...answerSet]
+
+    const choices = incorrectAnswers
+
+// TBC, set other options to fade away while correct answer fades in and enlarges.
+    const log = choices.map((choice, index) => (
+        choice === correctAnswer ?
+            <div key={index} className={answerState ? "correctAnswer" : "incorrectAnswer"}>
+                <h3>
+                    {`>: ${choice}`}
+                </h3>
+            </div>
+            :
+            <div key={index} className="incorrectAnswer">
+                <h3>
+                    {`>: ${choice}`}
+                </h3>
+            </div>
+    ))
+
+    return (
+        <div className="question">
+            <h5>{`Category: ${props.category}`}</h5>
+            <h2>{`Question: ${props.question.text}`}</h2>
+            {log}
+            <button
+                onClick={showAnswer}
+            >Show answer</button>
+
+        </div>
+
+    )
+}
